4e1e14323b768bf7808fb7c4b277e61cb7df67e5
Juan Göttling <juangottling@gmail.com>
2020-08-03 17:17:20 -0300

    finished delegation exercises

--- modulo003/patterns/src/test/java/com/globallogic/bootcamp/patterns/DelegationTest.java
+++ modulo003/patterns/src/test/java/com/globallogic/bootcamp/patterns/DelegationTest.java
@@ -1,5 +1,41 @@
 package com.globallogic.bootcamp.patterns;
 
-public class DelegationTest {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.globallogic.bootcamp.patterns.delegation.ElectricalOven;
+
+public class DelegationTest {
+	
+	private ElectricalOven oven;
+	
+	@BeforeEach
+	void init() {
+    	oven = new ElectricalOven();
+
+	}
+	
+	@Test
+	@DisplayName("At first power is 500")
+	void test1() {
+		assertEquals(500, oven.getPower());
+	}
+	
+	@Test
+	@DisplayName("when turned on power drops to 425")
+	void test2() {
+		oven.turnOn();
+		assertEquals(425, oven.getPower());
+	}
+	
+	@Test
+	@DisplayName("when led turned on power drops to 325")
+	void test3() {
+		oven.turnOn();
+		oven.turnLed();
+		assertEquals(325, oven.getPower());
+	}
 }
