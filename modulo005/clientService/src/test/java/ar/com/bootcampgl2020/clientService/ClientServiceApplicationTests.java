c8bd7492b328e7f6cbd421c4ef8061b1d467350a
Juan Göttling <31071051+jgottling@users.noreply.github.com>
2020-08-26 09:55:27 -0300

    finished ex 10

---
+++ modulo005/clientService/src/test/java/ar/com/bootcampgl2020/clientService/ClientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.bootcampgl2020.clientService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
