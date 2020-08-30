b3fcf47cb25e2c722031a3ea4aa5ad6f023cf2fc
Juan Göttling <31071051+jgottling@users.noreply.github.com>
2020-08-29 23:12:47 -0300

    books service

---
+++ modulo006/books-service/src/main/java/ar/com/bootcampgl2020/booksService/BooksServiceApplication.java
@@ -0,0 +1,13 @@
+package ar.com.bootcampgl2020.booksService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksServiceApplication.class, args);
+	}
+
+}
---
+++ modulo006/books-service/src/test/java/ar/com/bootcampgl2020/booksService/BooksServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.bootcampgl2020.booksService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
