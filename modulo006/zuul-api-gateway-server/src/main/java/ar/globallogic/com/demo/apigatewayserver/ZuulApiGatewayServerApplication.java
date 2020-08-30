87b8cb96c718d0272c2926837e7b1f001bf04df4
Juan Göttling <31071051+jgottling@users.noreply.github.com>
2020-08-30 17:19:00 -0300

    config sever working

--- modulo006/zuul-api-gateway-server/src/main/java/ar/globallogic/com/demo/apigatewayserver/ZuulApiGatewayServerApplication.java
+++ modulo006/zuul-api-gateway-server/src/main/java/ar/globallogic/com/demo/apigatewayserver/ZuulApiGatewayServerApplication.java
@@ -4,13 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-
 @EnableZuulProxy
 @SpringBootApplication
 public class ZuulApiGatewayServerApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ZuulApiGatewayServerApplication.class, args);
-	}
-	
+  public static void main(String[] args) {
+    SpringApplication.run(ZuulApiGatewayServerApplication.class, args);
+  }
 }
